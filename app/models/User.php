21139e88e5dfc35d39cf08c2713f37719628be1b
aegislp <ascarixs@hotmail.com>
2014-06-02 12:08:59 -0300

    ajuste usuario y package image

--- app/models/User.php
+++ app/models/User.php
@@ -59,7 +59,36 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 		return DB::table('users')->count();
 	}
 	 
-    
+    /**
+	 * Get the token value for the "remember me" session.
+	 *
+	 * @return string
+	 */
+	public function getRememberToken()
+	{
+		return $this->remember_token;
+	}
+
+	/**
+	 * Set the token value for the "remember me" session.
+	 *
+	 * @param  string  $value
+	 * @return void
+	 */
+	public function setRememberToken($value)
+	{
+		$this->remember_token = $value;
+	}
+
+	/**
+	 * Get the column name for the "remember me" token.
+	 *
+	 * @return string
+	 */
+	public function getRememberTokenName()
+	{
+		return 'remember_token';
+	}
     public function datos_validos($data)
     {
         $rules = array(
